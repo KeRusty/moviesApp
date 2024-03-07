d16fd71a0f98a6d943c3c19a83e3f5395a3ed54e
ChathuZenith <chathu.zenith@gmail.com>
2024-03-07 15:22:26 +0400

    Header added to navigation

--- moviesApp/src/Utils/Navigation/RootNavigation.tsx
+++ moviesApp/src/Utils/Navigation/RootNavigation.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// Components
+import Header from '../../Components/Header/Header';
+
 // Screens
 import Home from '../../Pages/Home/Home';
 
@@ -13,6 +16,14 @@ function RootNavigation() {
             <RootStack.Screen
                 name="HomeScreen"
                 component={Home}
+                options={{
+                    header: ({ navigation, route }) => (
+                        <Header
+                            navigation={navigation}
+                            route={route}
+                        />
+                    )
+                }}
             />
 
         </RootStack.Navigator>
