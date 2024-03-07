01b4acbba04ff546df94eac7e0602517e68c718c
ChathuZenith <chathu.zenith@gmail.com>
2024-03-07 10:07:18 +0400

    set up redux

--- moviesApp/index.js
+++ moviesApp/index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import store from './src/Utils/Redux/store';
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux'
+import 'react-native-gesture-handler';
 
-AppRegistry.registerComponent(appName, () => App);
+const FairBetsApp = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => FairBetsApp);
