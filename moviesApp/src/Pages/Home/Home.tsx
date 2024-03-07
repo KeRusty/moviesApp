f050018819900506decf4eaa9b76989c91d49b81
ChathuZenith <chathu.zenith@gmail.com>
2024-03-07 10:10:43 +0400

    more intial packages

---
+++ moviesApp/src/Pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+// Styles
+import styles from './Home-styles';
+
+function Home() {
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+
+export default Home;
