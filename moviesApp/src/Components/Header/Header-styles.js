38453136649acae9f0e5ddf60beb122adddc8a31
ChathuZenith <chathu.zenith@gmail.com>
2024-03-09 00:13:38 +0400

    header fixes

--- moviesApp/src/Components/Header/Header-styles.js
+++ moviesApp/src/Components/Header/Header-styles.js
@@ -1,14 +1,30 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import { appColors } from "../../Utils/Colors/colors";
 
 const styles = StyleSheet.create({
     container: {
-        height: 60,
+        height: 90,
         backgroundColor: appColors.gold,
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingHorizontal: 10
+        justifyContent: 'center',
+        paddingHorizontal: 10,
+        width: Dimensions.get('screen').width
+    },
+    headerContainer: {
+        width: '90%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerText: {
+        color: appColors.white,
+        fontWeight: 'bold',
+        fontSize: 20,
+        marginStart: -50
+    },
+    backButton: {
+        width: '10%',
+        marginStart: 10
     }
 })
 
--- moviesApp/src/Components/Header/Header.tsx
+++ moviesApp/src/Components/Header/Header.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { appColors } from '../../Utils/Colors/colors';
 
 // Styles
 import styles from './Header-styles';
 
 function Header({ navigation, route }: HeaderProps) {
+    console.log(route.name)
     return (
-        <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
+            {route.name !== "HomeScreen" &&
+                <TouchableOpacity
+                    style={styles.backButton}
+                    onPress={() => navigation.goBack()}>
+                    <Icon
+                        name="arrow-back-outline"
+                        size={30}
+                        color={appColors.white}
+                    />
+                </TouchableOpacity>
+            }
+            {route.name !== "HomeScreen" &&
+                <View style={styles.headerContainer}>
+                    <Text style={styles.headerText}>{route.name === "MovieDetailsScreen" ? "Details" : "Home"}</Text>
+                </View>
+            }
 
-        </View>
+        </SafeAreaView>
     )
 }
 
